868722a2d374057974bb6b1fbd4c654e07c70177
syfll <lintao.yu@dianping.com>
2017-10-27 13:49:59 +0800

    Initial Commit

---
+++ JFTFaceDetection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JFTFaceDetection
+//
+//  Created by syfll on 2017/10/27.
+//  Copyright © 2017年 syfll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JFTFaceDetection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JFTFaceDetection
+//
+//  Created by syfll on 2017/10/27.
+//  Copyright © 2017年 syfll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
